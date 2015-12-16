07eae1b7ff70ce04fd1ca95fb3f7c67b2b848b74
Oliv <olivier-marin@hotmail.fr>
2015-12-16 17:43:17 +0100

    Creation TODO

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,14 @@
 package main;
 
+/**
+ * @TODO Creation BDD
+ * @TODO Connexion BDD / Appli
+ * @TODO Algo Comparaison souple
+ * @TODO Algo Comparaison strict
+ * @TODO IHM test
+ * @TODO Implementation design pattern Decorator (facultatif)
+ */
+
 public class Main {
 
     public static void main(String[] args) {
