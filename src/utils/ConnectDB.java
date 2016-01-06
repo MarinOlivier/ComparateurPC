1022e643f6cc4ce83b5379fcb6ccf1759786a833
jose-t <josetarsitano@jls.local>
2016-01-06 16:00:00 +0100

    Second try
    ConnectDB

--- src/utils/ConnectDB.java
+++ src/utils/ConnectDB.java
@@ -4,14 +4,6 @@
 package utils;
 
 import java.sql.*;
-//import java.sql.Connection;
-//import java.sql.DriverManager;
-import java.sql.SQLException;
-
-import java.sql.DriverManager;
-import java.sql.SQLException;
-//import javax.servlet.http.HttpServletRequest;
-
 import com.mysql.jdbc.Connection;
 /**
  * @author josetarsitano
@@ -21,17 +13,14 @@ public class ConnectDB {
     public static void startConnection() {
         Connection conn = null;
         try {
-            // The newInstance() call is a work around for some
-            // broken Java implementations
             conn = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:8888/projet_poo?" +
                             "user=root&password=");
-
 
             Class.forName("com.mysql.jdbc.Driver").newInstance();
             
             /* Création de l'objet gérant les requêtes */
             Statement statement = conn.createStatement();
-            ResultSet res = statement.executeQuery( "SELECT * FROM user;");
+            ResultSet res = statement.executeQuery("SELECT * FROM user;");
         } catch (Exception ex) {
             // handle any errors
             System.out.println("SQLException: " + ex.getMessage());
