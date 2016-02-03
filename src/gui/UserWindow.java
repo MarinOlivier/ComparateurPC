131e12a46d783b2d3bb72ddcb5f6f10f6111c90c
Oliv <olivier-marin@hotmail.fr>
2016-02-03 12:12:10 +0100

    Update GUI UserWindow

--- src/gui/UserWindow.java
+++ src/gui/UserWindow.java
@@ -20,18 +20,29 @@ import javax.xml.stream.events.Characters;
  */
 public class UserWindow extends JFrame {
     private JTextField _usernameField;
+    private JLabel _usernameLabel;
     private JPasswordField _passwordField;
+    private JLabel _passwordLabel;
     private JPanel _mainPanel;
     private JButton _connexion;
+    private JPanel _formPanel;
+    private JPanel _subPanel;
+    private JLabel _msgAlert;
 
     private String _password;
 
-	public UserWindow() {
-		setSize(300, 300);
-        _mainPanel = new JPanel(new GridLayout(2, 2));
-        _usernameField = new JTextField("");
-        _passwordField = new JPasswordField();
+    public UserWindow() {
+        setSize(300, 170);
+        setResizable(false);
+        _formPanel = new JPanel(new GridLayout(2, 2, 2, 2));
+        _subPanel = new JPanel(new GridLayout(2, 2, 2, 2));
+        _usernameField = new JTextField(10);
+        _passwordField = new JPasswordField(10);
         _connexion = new JButton("Connexion");
+
+        _msgAlert = new JLabel("");
+        _msgAlert.setVisible(false);
+        _msgAlert.setForeground(Color.RED);
 
         _connexion.addActionListener(new ActionListener() {
             @Override
@@ -39,24 +50,40 @@ public class UserWindow extends JFrame {
                 if (_passwordField.getPassword().length != 0) {
                     _password = String.valueOf(_passwordField.getPassword());
                     System.out.println(_password);
-                } else
-                    System.out.println("Password vide.");
+                } else {
+                    _msgAlert.setText("Nom d'utilisateur ET/OU Mot de passe vide");
+                    _msgAlert.setVisible(true);
+                }
             }
         });
 
-		addWindowListener(new WindowAdapter() {
-			@Override
-			public void windowClosed(WindowEvent e) {
-				
-				
-			}
-		});
-        // LOL
-        _mainPanel.add(new JLabel("Nom d'utilisateur :"));
-        _mainPanel.add(_usernameField);
-        _mainPanel.add(new JLabel("Mot de passe :"));
-        _mainPanel.add(_passwordField);
-        _mainPanel.add(_connexion);
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosed(WindowEvent e) {
+
+
+            }
+        });
+
+        _usernameLabel = new JLabel("Nom d'utilisateur :");
+        _usernameLabel.setLabelFor(_usernameField);
+        _passwordLabel = new JLabel("Mot de passe :");
+        _passwordLabel.setLabelFor(_passwordField);
+
+        _formPanel.add(_usernameLabel);
+        _formPanel.add(_usernameField);
+        _formPanel.add(_passwordLabel);
+        _formPanel.add(_passwordField);
+
+        _subPanel.add(_msgAlert, BorderLayout.NORTH);
+        _subPanel.add(_connexion, BorderLayout.SOUTH);
+
+        _mainPanel = new JPanel();
+        _mainPanel.add(_formPanel, BorderLayout.NORTH);
+        _mainPanel.add(_subPanel, BorderLayout.SOUTH);
+
+        _mainPanel.setBorder(BorderFactory.createTitledBorder("Connexion"));
+
         add(_mainPanel, BorderLayout.CENTER);
 
         setVisible(true);
